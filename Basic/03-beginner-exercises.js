e4827544198958aafcd5e204d58299bc10ac7432
Javier <trujax@gmail.com>
2025-10-21 19:28:02 +0200

    @

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -5,20 +5,91 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 
 // 1. Escribe un comentario en una línea
 
+// Esto es un comentario
+
 // 2. Escribe un comentario en varias líneas
+
+/*
+Comentario
+Multi
+Línea
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
+let entero = 0
+let decimal = 2.3
+let string = "string"
+let boolean = false
+let undefinedVal
+let nullVal = null
+let simbolo = Symbol("simbolosimbolico")
+let mibigint = 23234234421432134n
+
 // 4. Imprime por consola el valor de todas las variables
+
+console.log(entero)
+console.log(decimal)
+console.log(string)
+console.log(boolean)
+console.log(undefinedVal)
+console.log(nullVal)
+console.log(simbolo)
+console.log(mibigint)
 
 // 5. Imprime por consola el tipo de todas las variables
 
+console.log(typeof(entero))
+console.log(typeof(decimal))
+console.log(typeof(string))
+console.log(typeof(boolean))
+console.log(typeof(undefinedVal))
+console.log(typeof(nullVal))
+console.log(typeof(simbolo))
+console.log(typeof(mibigint))
+
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+ entero = 4
+ decimal = 5.3
+ string = "stringgg"
+ boolean = true
+ undefinedVal
+ nullVal = null
+ simbolo = Symbol("simbolosimbolicosimbolizador")
+ mibigint = 2323423442143212342349876423n
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
+entero = 5.3
+decimal = "string"
+string = 2
+boolean = null
+undefinedVal
+nullVal = Symbol("simbolosimbolicosimbolizador")
+simbolo = true
+mibigint = 2323
+
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
+const enteroC = 0
+const decimalC = 2.3
+const stringC = "string"
+const booleanC = false
+// const undefinedValC
+const nullValC = null
+const simboloC = Symbol("simbolosimbolico")
+const mibigintC = 23234234421432134n
+
 // 9. A continuación, modifica los valores de las constantes
+
+// const enteroC = 0
+// const decimalC = 2.3
+// const stringC = "string"
+// const booleanC = false
+// const undefinedValC
+// const nullValC = null
+// const simboloC = Symbol("simbolosimbolico")
+// const mibigintC = 23234234421432134n
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
